21da10d9065d5dea7a99c1ec6a9dfb28863b4808
Anant <anant.mathur007@gmail.com>
2021-05-02 13:17:45 +0530

    implement JSON-server and install router

--- src/App.vue
+++ src/App.vue
@@ -40,20 +40,54 @@ export default {
   methods: {
     // Add a new task
     async addTask(task) {
-      this.tasks = [...this.tasks, task];
+      const res = await fetch("api/tasks", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(task),
+      });
+
+      const data = await res.json();
+
+      this.tasks = [...this.tasks, data];
     },
 
     // Toggle reminder property of a specific task object
     async toggleReminder(id) {
+      const taskToToggle = await this.fetchTask(id);
+
+      console.log(taskToToggle);
+
+      const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+
+      const res = await fetch(`api/tasks/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(updTask),
+      });
+
+      const data = await res.json();
+
+      console.log(data);
+
       this.tasks = this.tasks.map((task) =>
-        task.id === id ? { ...task, reminder: !task.reminder } : task
+        task.id === id ? { ...task, reminder: data.reminder } : task
       );
     },
 
     // Delete a task by id
     async deleteTask(id) {
       if (confirm("Are you sure?")) {
-        this.tasks = this.tasks.filter((task) => task.id !== id);
+        const res = await fetch(`api/tasks/${id}`, {
+          method: "DELETE",
+        });
+
+        res.status === 200
+          ? (this.tasks = this.tasks.filter((task) => task.id !== id))
+          : alert("Error!");
       }
     },
 
@@ -61,30 +95,29 @@ export default {
     async toggleAddTask() {
       this.showAddTask = !this.showAddTask;
     },
+
+    // Fetch all tasks from backend on load
+    async fetchTasks() {
+      const res = await fetch("api/tasks");
+
+      const data = await res.json();
+
+      return data;
+    },
+
+    // Fetch a specific task from backend
+    async fetchTask(id) {
+      const res = await fetch(`api/tasks/${id}`);
+
+      const data = await res.json();
+
+      return data;
+    },
   },
 
   // Lifecycle method, similar to useEffect (make requests here, initlise state etc)
-  created() {
-    this.tasks = [
-      {
-        id: "1",
-        text: "Doctors Appointment",
-        day: "March 5th at 2:30pm",
-        reminder: true,
-      },
-      {
-        id: "2",
-        text: "Meeting with boss",
-        day: "March 6th at 1:30pm",
-        reminder: true,
-      },
-      {
-        id: "3",
-        text: "Food shopping",
-        day: "March 7th at 2:00pm",
-        reminder: false,
-      },
-    ];
+  async created() {
+    this.tasks = await this.fetchTasks();
   },
 };
 </script>
